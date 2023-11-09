b6368a4a45a45e44db6cfbbc5c4be81d27d58457
Andolo-64 <90448671+Andolo-64@users.noreply.github.com>
2023-11-09 16:28:19 +0100

    php

--- Client.php
+++ Client.php
@@ -1,1 +1,31 @@
 //©A,O Høgestøl 2023
+<!--
+©A,O Høgestøl 2023
+-->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+</head>
+
+<body>
+    
+    <link rel="stylesheet" href="Client.css">
+<div class = "container">
+    
+<h1>Client<h1>
+
+    <button id="generateBtn" onclick="genBtn()"> button </button>
+         
+    <?php   
+    
+    echo"Hello";
+    
+    ?>
+</div>
+<script src="Client.js" ></script>
+</body>
+</html>
